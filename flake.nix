46890c8e969f555ebf5dc8a939c6bfd2b510fd48
Gavin Mendel-Gleason <jacobian@gmail.com>
2025-01-27 15:47:59 +0100

    Adding updated lock and some nix stuff

--- flake.nix
+++ flake.nix
@@ -3,22 +3,24 @@
     nixpkgs.url = "github:nixOS/nixpkgs?ref=nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     pyproject-nix = {
-      url = "github:pyproject-nix/pyproject.nix";
+      url = "github:pyproject-nix/pyproject.nix/e14a14";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
+      url = "github:pyproject-nix/uv2nix/b371dcc";
       inputs.pyproject-nix.follows = "pyproject-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
+      url = "github:pyproject-nix/build-system-pkgs/22d5f5";
       inputs.pyproject-nix.follows = "pyproject-nix";
       inputs.uv2nix.follows = "uv2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    pyproject-nix-overrides.url = "github:vectorlink-ai/pyproject-nix-overrides";
   };
 
 
@@ -28,6 +30,7 @@
       pyproject-nix,
       uv2nix,
       pyproject-build-systems,
+      pyproject-nix-overrides,
       ...
   }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -44,11 +47,11 @@
         };
         pythonSet = pkgs.callPackage ./nix/uv-python.nix {
           inherit pyproject-nix pyproject-build-systems workspace;
+          # pyproject-overrides = pyproject-nix-overrides.overrides pkgs;
         };
       in
       {
         devShells = {
-          #default = pkgs.callPackage ./nix/shell.nix {inherit python3-base; };
           default = pkgs.callPackage ./nix/uv-shell.nix {
             inherit workspace pythonSet;
           };
@@ -56,3 +59,4 @@
       }
   );
 }
+
